7f177cddc7a4cde0c28fb26b2fd24a9932b7aa99
unknown <kuku9820@o365.yonsei.ac.kr>
2023-08-31 22:03:06 +0900

    need debugging

--- 전재화_로봇청소기4_user.cpp
+++ 전재화_로봇청소기4_user.cpp
@@ -1,25 +1,69 @@
 ﻿extern void scan(int floorState[3][3]);
 extern int move(void);
 extern void turn(int mCommand);
+#include <iostream>
 
-#define MAX 150
-#define INIT_POS 75
-#define SCORE_MAX 99999999
-
-int currDir, currY, currX;
-int dy[4] = { -1,0,+1,0 };
-int dx[4] = { 0,-1,0,+1 };
-enum Dir { UP, LEFT, DOWN, RIGHT }; //4로 나눈 나머지 연산을 통해 방향 전환 후 진행방향을 알 수 있음
-enum Cost { SCAN = 20, TURN = 15, MOVE = 10 };
-
+/////////////// Data Structure //////////////////
 struct Coor
 {
     int y, x, dir, score;
 };
 
+struct Diff
+{
+    int dy, dx, status;
+};
+
+struct Candidate
+{
+    int y, x, dir;
+    bool valid;
+};
+
+////////////////////////////////////////////////
+
+//////////////////// API ///////////////////////
+static int constexpr QueueMax = 64 * 64 * 4 + 10;
+
+template <typename T>
+struct Queue
+{
+    T data[QueueMax];
+    int front, rear;
+
+    Queue() : front(0), rear(0)
+    {
+
+    }
+
+    void init(void)
+    {
+        front = 0, rear = 0;
+    }
+
+    bool empty(void)
+    {
+        return front == rear;
+    }
+
+    int size(void)
+    {
+        return rear;
+    }
+
+    void push(T src)
+    {
+        data[rear++] = src;
+    }
+
+    T pop(void)
+    {
+        return data[front++];
+    }
+};
+
 struct PriorityQueue
 {
-    static int constexpr QueueMax = 64 * 64 + 10;
     Coor arr[QueueMax];
     int size;
 
@@ -92,200 +136,446 @@ struct PriorityQueue
         return popVal;
     }
 };
+/////////////////////////////////////////////////
 
-struct Robot
+///////////// global variable ///////////////////
+#define MAX 150
+#define N 64
+#define INIT_POS 75
+#define VALID true
+#define INVALID false
+#define THRESHOLD 50
+
+enum Dir { UP, LEFT, DOWN, RIGHT }; //4로 나눈 나머지 연산을 통해 방향 전환 후 진행방향을 알 수 있음
+enum Cost { SCAN = 20, TURN = 15, MOVE = 10 };
+enum Status { NOT_BLOCK = 0, BLOCK, NEED_SCAN };
+
+int currDir, currY, currX;
+int dy[4] = { -1,0,+1,0 };
+int dx[4] = { 0,-1,0,+1 };
+int map[MAX][MAX];
+int compMap[MAX][MAX];
+long long int visit[MAX][MAX];
+long long int visitCheck;
+bool cleaned[MAX][MAX];
+int path[MAX][MAX];
+int size;
+Coor min_coor;
+int rotateMap[4][MAX][MAX];
+Queue<Candidate> candidateQ;
+int cddQSize;
+int Y, X;
+/////////////////////////////////////////////////
+
+void initMap(void)
 {
-    enum status
+    size = 0;
+    for (int i = 0; i < MAX; i++)
     {
-        NOT_BLOCK = 0, BLOCK, NEED_CLEANING, NEED_SCAN
-    };
-    
-    int map[MAX][MAX];
-    int visit[MAX][MAX], visitCheck;
-    bool cleaned[MAX][MAX];
-    int path[MAX][MAX];
-    int min, size;
-    Coor min_coor;
+        for (int j = 0; j < MAX; j++)
+        {
+            map[i][j] = NEED_SCAN;
+            cleaned[i][j] = false;
+        }
+    }
+    cleaned[INIT_POS][INIT_POS] = true;
+}
 
-    Robot() : min(SCORE_MAX), size(0)
+void initCompMap(void)
+{
+    size = 0;
+    for (int i = 0; i < MAX; i++)
     {
+        for (int j = 0; j < MAX; j++)
+        {
+            compMap[i][j] = NEED_SCAN;   
+            cleaned[i][j] = false;
+        }
+    }
+    cleaned[INIT_POS][INIT_POS] = true;
+
+
+    for (int y = 0, sy = 0; y < MAX; ++y, ++sy)
+        for (int x = 0, sx = 0; x < MAX; ++x, ++sx)
+            rotateMap[UP][sy][sx] = map[y][x];
+
+    for (int y = 0, sx = MAX - 1; y < MAX; ++y, --sx)
+        for (int x = 0, sy = 0; x < MAX; ++x, ++sy)
+            rotateMap[LEFT][sy][sx] = map[y][x];
+
+    for (int y = 0, sy = MAX - 1; y < MAX; ++y, --sy)
+        for (int x = 0, sx = MAX - 1; x < MAX; ++x, --sx)
+            rotateMap[DOWN][sy][sx] = map[y][x];
+
+    for (int y = 0, sx = 0; y < MAX; ++y, ++sx)
+        for (int x = 0, sy = MAX - 1; x < MAX; ++x, --sy)
+            rotateMap[RIGHT][sy][sx] = map[y][x];
+
+    //for (int dir = 0; dir < 4; dir++)
+    //{
+    //    for (int i = 0; i < MAX; i++)
+    //    {
+    //        for (int j = 0; j < MAX; j++)
+    //        {
+    //            if (rotateMap[dir][i][j] == NEED_SCAN)
+    //                std::cout << " ";
+    //            else
+    //                std::cout << rotateMap[dir][i][j];
+    //        }
+    //        std::cout << std::endl;
+    //    }
+    //}
+
+    //int a = 10;
+}
+
+Coor pop(int src[][MAX])
+{
+    cleaned[min_coor.y][min_coor.x] = true;
+    --size;
+    return min_coor;
+}
+
+bool needScan(int src[][MAX])
+{
+    for (int dir = 0; dir < 4; dir++)
+    {
+        if (src[currY + dy[dir]][currX + dx[dir]] == NEED_SCAN)
+            return true;
+    }
+    return false;
+}
+
+inline void mapping(int floorState, int& map, bool cleaned)
+{
+    if (map == NOT_BLOCK && cleaned == false) //이미 카운팅 한 경우
+        return;
+    map = floorState;
+    if (map == NOT_BLOCK && cleaned == false)  
+        ++size;  
+}
+
+void mapUpdate(int floorState[3][3], int dest[][MAX])
+{
+    int baseY = currY - 1;
+    int baseX = currX - 1;
+    switch (currDir)
+    {
+    case UP:
+        for (int y = 0, sy = 0; y <= 2; y++, sy++)
+            for (int x = 0, sx = 0; x <= 2; x++, sx++)
+                mapping(floorState[y][x], dest[baseY + sy][baseX + sx], cleaned[baseY + sy][baseX + sx]);
+        break;
+    case LEFT:
+        for (int y = 0, sx = 0; y <= 2; y++, sx++)
+            for (int x = 0, sy = 2; x <= 2; x++, sy--)
+                mapping(floorState[y][x], dest[baseY + sy][baseX + sx], cleaned[baseY + sy][baseX + sx]);
+        break;
+    case DOWN:
+        for (int y = 0, sy = 2; y <= 2; y++, sy--)
+            for (int x = 0, sx = 2; x <= 2; x++, sx--)
+                mapping(floorState[y][x], dest[baseY + sy][baseX + sx], cleaned[baseY + sy][baseX + sx]);
+        break;
+    case RIGHT:
+        for (int y = 0, sx = 2; y <= 2; y++, sx--)
+            for (int x = 0, sy = 0; x <= 2; x++, sy++)
+                mapping(floorState[y][x], dest[baseY + sy][baseX + sx], cleaned[baseY + sy][baseX + sx]);
+        break;
+    }
+}
+
+void update(int src[][MAX])
+{
+    ++visitCheck;
+    PriorityQueue pq;
+    pq.push({ currY, currX, currDir, 0 });
+    visit[currY][currX] = visitCheck;
+    path[currY][currX] = currDir;
+
+    while (!pq.empty())
+    {
+        Coor now = pq.pop();
+
+        if (src[now.y][now.x] == NOT_BLOCK && cleaned[now.y][now.x] == false)
+        {
+            min_coor = now;
+            break;
+        }
+
+        for (int diff = 0; diff < 4; diff++)
+        {
+            int ndir = (now.dir + diff) % 4;
+            int ny = now.y + dy[ndir];
+            int nx = now.x + dx[ndir];
+            if (src[ny][nx] == NOT_BLOCK && visit[ny][nx] != visitCheck)
+            {
+                pq.push({ ny, nx, ndir, diff == 0 ? now.score + Cost::MOVE : now.score + Cost::TURN + Cost::MOVE });
+                visit[ny][nx] = visitCheck;
+                path[ny][nx] = ndir;
+            } 
+        }
+    }
+}
+
+void FollowPath(Coor now)
+{
+    if (now.y == currY && now.x == currX)
+        return;
+
+    int opDir = (path[now.y][now.x] + 2) % 4;
+    int ny = now.y + dy[opDir];
+    int nx = now.x + dx[opDir];
+
+    FollowPath({ ny, nx });
+
+    int diffDir = (path[now.y][now.x] - path[ny][nx] + 4) % 4;
+    if(diffDir != 0)
+        turn(diffDir);
+    move();
+}
+
+bool inline isValidRange(int y, int x)
+{
+    return y >= 0 && y < MAX && x >= 0 && x < MAX;
+}
+
+bool isValid(Queue<Diff> q, int y, int x, int dir)
+{
+    int ny, nx;
+    for (int idx = 0; idx < q.size(); idx++)
+    {
+        Diff now = q.data[idx];
+        ny = y + now.dy;
+        nx = x + now.dx;
+        if (isValidRange(ny, nx) == false || rotateMap[dir][ny][nx] == NEED_SCAN || rotateMap[dir][ny][nx] != now.status)
+            return false;
+    }
+    return true;
+}
+
+void copyMap(int src[][MAX], int pivotY, int pivotX)
+{
+    int dy = currY - pivotY;
+    int dx = currX - pivotX;
+    bool temp[MAX][MAX];
+    for (int y = 0; y < MAX; y++)
+    {
+        for (int x = 0; x < MAX; x++)
+        {
+            if (src[y][x] == NEED_SCAN || isValidRange(y + dy, x + dx) == false)
+                continue;
+            compMap[y + dy][x + dx] = src[y][x];
+            temp[y + dy][x + dx] = cleaned[y][x];
+        }
+    }
+
+    for (int y = 0; y < MAX; y++)
+    {
+        for (int x = 0; x < MAX; x++)
+        {
+            cleaned[y][x] = temp[y][x];
+        }
+    }
+}
+
+bool detectStatus(bool isFirst)
+{
+    if (isFirst == true)
+    {
+        Y = currY;
+        X = currX;
+    }
+    //compMap을 BFS로 돌면서 4방향 diff Queue에 담기
+    ++visitCheck;
+    Queue<Diff> diffQ;
+    diffQ.push({ 0, 0, compMap[currY][currX] });
+    visit[currY][currX] = visitCheck;
+
+    while (!diffQ.empty())
+    {
+        Diff now = diffQ.pop();
+        int nowY = Y + now.dy;
+        int nowX = X + now.dx;
+
+        for (int dir = 0; dir < 4; dir++)
+        {
+            int ny = nowY + dy[dir];
+            int nx = nowX + dx[dir];
+            if (isValidRange(ny, nx) == true && compMap[ny][nx] != NEED_SCAN && visit[ny][nx] != visitCheck)
+            {
+                int nstatus = compMap[ny][nx];              
+                diffQ.push({ now.dy + dy[dir], now.dx + dx[dir], nstatus });
+                visit[ny][nx] = visitCheck;
+            }
+        }
+    }
+    // 첫번째 detect status 면 map과 diff queue를 비교하면서, 가능한 후보군 개수 추리기
+    if (isFirst == true)
+    {
+        candidateQ.init();
+        cddQSize = 0;
         for (int i = 0; i < MAX; i++)
         {
             for (int j = 0; j < MAX; j++)
             {
-                map[i][j] = NEED_SCAN;
-                visit[i][j] = 0;
-                cleaned[i][j] = false;
-            }
-        }
-        cleaned[INIT_POS][INIT_POS] = true;
-    }
-
-    Coor pop(void)
-    {
-        map[min_coor.y][min_coor.x] = NOT_BLOCK;
-        cleaned[min_coor.y][min_coor.x] = true;
-        --size;
-        return min_coor;
-    }
-
-    bool needScan(void)
-    {
-        for (int dir = 0; dir < 4; dir++)
-        {
-            if (map[currY + dy[dir]][currX + dx[dir]] == NEED_SCAN)
-                return true;
-        }
-        return false;
-    }
-
-    inline void mapping(int floorState, int& map, bool cleaned)
-    {
-        if (map == status::NEED_CLEANING)
-            return;
-        map = floorState;
-        if (map == status::NOT_BLOCK && cleaned == false)
-        {
-            map = status::NEED_CLEANING;
-            ++size;
-        }
-    }
-
-    void mapUpdate(int floorState[3][3])
-    {
-        int baseY = currY - 1;
-        int baseX = currX - 1;
-        if (currDir == Dir::UP)
-        {
-            for (int y = 0, sy = 0; y <= 2; y++, sy++)
-            {
-                for (int x = 0, sx = 0; x <= 2; x++, sx++)
+                for (int dir = 0; dir < 4; dir++)
                 {
-                    mapping(floorState[y][x], map[baseY + sy][baseX + sx], cleaned[baseY + sy][baseX + sx]);
-                }
-            }
-        }
-        else if (currDir == Dir::LEFT)
-        {
-            for (int y = 0, sx = 0; y <= 2; y++, sx++)
-            {
-                for (int x = 0, sy = 2; x <= 2; x++, sy--)
-                {
-                    mapping(floorState[y][x], map[baseY + sy][baseX + sx], cleaned[baseY + sy][baseX + sx]);
-                }
-            }
-        }
-        else if (currDir == Dir::DOWN)
-        {
-            for (int y = 0, sy = 2; y <= 2; y++, sy--)
-            {
-                for (int x = 0, sx = 2; x <= 2; x++, sx--)
-                {
-                    mapping(floorState[y][x], map[baseY + sy][baseX + sx], cleaned[baseY + sy][baseX + sx]);
-                }
-            }
-        }
-        else if (currDir == Dir::RIGHT)
-        {
-            for (int y = 0, sx = 2; y <= 2; y++, sx--)
-            {
-                for (int x = 0, sy = 0; x <= 2; x++, sy++)
-                {
-                    mapping(floorState[y][x], map[baseY + sy][baseX + sx], cleaned[baseY + sy][baseX + sx]);
+                    if (isValid(diffQ, i, j, dir))
+                    {
+                        candidateQ.push({ i,j,dir,VALID });
+                        ++cddQSize;
+                    }
                 }
             }
         }
     }
-
-    void update(void)
+    else
     {
-        ++visitCheck;
-        min = SCORE_MAX;
-        PriorityQueue pq;
-        pq.push({ currY, currX, currDir, 0 });
-        visit[currY][currX] = visitCheck;
-
-        while (!pq.empty())
+        for (int idx = 0; idx < candidateQ.size(); idx++)
         {
-            Coor now = pq.pop();
-
-            if (map[now.y][now.x] == NEED_CLEANING)
+            Candidate& now = candidateQ.data[idx];
+            if (now.valid == INVALID)
+                continue;
+            if (isValid(diffQ, now.y, now.x, now.dir) == false)
             {
-                min_coor = now;
+                now.valid = INVALID;
+                --cddQSize;
+            }
+        }
+    }
+    
+    if (cddQSize == 1)
+    {
+        Candidate status;
+        for (int idx = 0; idx < candidateQ.size(); idx++)
+        {
+            Candidate& now = candidateQ.data[idx];
+            if (now.valid == VALID)
+            {
+                status = now;
                 break;
             }
-
-            for (int diff = 0; diff < 4; diff++)
-            {
-                int ndir = (now.dir + diff) % 4;
-                int ny = now.y + dy[ndir];
-                int nx = now.x + dx[ndir];
-                if (map[ny][nx] != BLOCK && map[ny][nx] != NEED_SCAN && visit[ny][nx] != visitCheck)
-                {
-                    pq.push({ ny, nx, ndir, diff == 0 ? now.score + Cost::MOVE : now.score + Cost::TURN + Cost::MOVE });
-                    visit[ny][nx] = visitCheck;
-                    path[ny][nx] = ndir;
-                } 
-            }
         }
+
+        copyMap(rotateMap[status.dir], status.y, status.x);
+        return true;
     }
+    else
+        return false;
+}
 
-    void FollowPath(Coor now, Coor toGo)
-    {
-        if (now.y == currY && now.x == currX)
-        {
-            int diffDir = (path[toGo.y][toGo.x] - currDir + 4) % 4;
-            if (diffDir != 0)
-                turn(diffDir);
-            move();
-            return;
-        }
-        int opDir = (path[now.y][now.x] + 2) % 4;
-        int ny = now.y + dy[opDir];
-        int nx = now.x + dx[opDir];
-
-        FollowPath({ ny, nx }, now);
-
-        int diffDir = (path[toGo.y][toGo.x] - path[now.y][now.x] + 4) % 4;
-        if(diffDir != 0)
-            turn(diffDir);
-        move();
-    }
-};
 
 void init(void)
 {
+    //do nothing
+}
 
+void makeMap(void)
+{
+    int floorState[3][3];
+    Coor dest;
+
+    do
+    {
+        if (needScan(map) == true)
+        {
+            scan(floorState);
+            mapUpdate(floorState, map);
+        }
+
+        if (size == 0)
+            break;
+
+        update(map);
+
+        dest = pop(map);
+
+        FollowPath(dest);
+
+        currDir = path[dest.y][dest.x];
+        currY = dest.y;
+        currX = dest.x;
+    } while (true);
+}
+
+void reuseMap(void)
+{
+    int floorState[3][3];
+    Coor dest;
+    bool isFirst = true;
+
+    do
+    {
+        int num = 0;
+        while (num++ < THRESHOLD)
+        {
+            if (needScan(compMap) == true)
+            {
+                scan(floorState);
+                mapUpdate(floorState, compMap);
+            }
+
+            if (size == 0)
+                break;
+
+            update(compMap);
+
+            dest = pop(compMap);
+
+            FollowPath(dest);
+
+            currDir = path[dest.y][dest.x];
+            currY = dest.y;
+            currX = dest.x;
+        }
+
+        for (int i = 0; i < MAX; i++)
+        {
+            for (int j = 0; j < MAX; j++)
+            {
+                if (compMap[i][j] == NEED_SCAN)
+                    std::cout << " ";
+                else
+                    std::cout << compMap[i][j];
+            }
+            std::cout << std::endl;
+        }
+
+        if (detectStatus(isFirst) == true)
+            break;
+                
+        isFirst = false;
+    } while (true);
+
+    while (size != 0)
+    {
+        update(compMap);
+
+        dest = pop(compMap);
+
+        FollowPath(dest);
+
+        currDir = path[dest.y][dest.x];
+        currY = dest.y;
+        currX = dest.x;
+    }
 }
 
 void cleanHouse(void)
 {
+    static int subTask = 0;
     currY = INIT_POS, currX = INIT_POS, currDir = Dir::UP;
-    int floorState[3][3];
-    Robot robot;
-    Coor dest;
-    
-    do 
+
+    if (subTask % 10 == 0)
     {
-        if (robot.needScan() == true)
-        {
-            scan(floorState);
-            robot.mapUpdate(floorState);
-        }
-        robot.update();
-
-        if (robot.size == 0)
-            break;
-
-        dest = robot.pop();
-        //path 저장된거에 따라 move 및 turn호출
-        int opDir = (robot.path[dest.y][dest.x] + 2) % 4;
-        int ny = dest.y + dy[opDir];
-        int nx = dest.x + dx[opDir];
-        robot.FollowPath({ny, nx}, dest);
-        //currDir, currY, currX 갱신
-        currDir = robot.path[dest.y][dest.x];
-        currY = dest.y;
-        currX = dest.x;
-    } while (true);
+        initMap();
+        makeMap();
+    }
+    else
+    {
+        initCompMap();
+        reuseMap();
+    }
+    
+    ++subTask;
 }
